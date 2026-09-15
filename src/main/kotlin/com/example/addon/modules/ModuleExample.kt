8058a7b57a59c4fb3607522e8a577facb11da495
Izuna Seikatsu <izuna.seikatsu@ccbluex.net>
2026-09-15 15:58:49 +0200

    refactor(ModuleExample): wait ticks instead of counting them

--- src/main/kotlin/com/example/addon/modules/ModuleExample.kt
+++ src/main/kotlin/com/example/addon/modules/ModuleExample.kt
@@ -2,6 +2,7 @@ package com.example.addon.modules
 
 import com.example.addon.ExampleCategories
 import net.ccbluex.liquidbounce.event.tickHandler
+import net.ccbluex.liquidbounce.event.waitTicks
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.ccbluex.liquidbounce.utils.client.chat
 
@@ -14,15 +15,9 @@ object ModuleExample : ClientModule("Example", ExampleCategories.EXAMPLE) {
     private val interval by int("Interval", 100, 20..600, "ticks")
     private val greeting by text("Greeting", "Hello from the example add-on")
 
-    private var ticks = 0
-
     @Suppress("unused")
     private val tickHandler = tickHandler {
-        if (++ticks < interval) {
-            return@tickHandler
-        }
-
-        ticks = 0
+        waitTicks(interval)
         chat(greeting)
     }
 
